fa153a81452e31919f162509051a7352662388ec
polesye <s2pak.anton@gmail.com>
2014-08-05 10:10:40 +0300

    Add tooltip manager in Studio.

---
+++ cms/static/js/tooltip_manager.js
@@ -0,0 +1,79 @@
+define(['jquery', 'underscore'], function($, _) {
+    'use strict';
+    var TooltipManager = function (element) {
+        this.element = $(element);
+        // If tooltip container already exist, use it.
+        this.tooltip = $('div.' + this.className.split(/\s+/).join('.'));
+        // Otherwise, create new one.
+        if (!this.tooltip.length) {
+            this.tooltip = $('<div />', {
+                'class': this.className
+            }).css('opacity', 0).appendTo(this.element);
+        }
+
+        _.bindAll(this);
+        this.bindEvents();
+    };
+
+    TooltipManager.prototype = {
+        // Space separated list of class names for the tooltip container.
+        className: 'tooltip',
+        SELECTOR: '[data-tooltip]',
+
+        bindEvents: function () {
+            this.element.on({
+                'mouseover.TooltipManager': this.showTooltip,
+                'mousemove.TooltipManager': this.moveTooltip,
+                'mouseout.TooltipManager': this.hideTooltip,
+                'click.TooltipManager': this.hideTooltip
+            }, this.SELECTOR);
+        },
+
+        getCoords: function (pageX, pageY) {
+            return {
+                'left': pageX - 0.5 * this.tooltip.outerWidth(),
+                'top': pageY - (this.tooltip.outerHeight() + 15)
+            };
+        },
+
+        show: function () {
+            this.tooltip.css('opacity', 1);
+        },
+
+        hide: function () {
+            this.tooltip.css('opacity', 0);
+        },
+
+        showTooltip: function(event) {
+            var tooltipText = $(event.currentTarget).attr('data-tooltip');
+            this.tooltip
+                .html(tooltipText)
+                .css(this.getCoords(event.pageX, event.pageY));
+
+            if (this.tooltipTimer) {
+                clearTimeout(this.tooltipTimer);
+            }
+            this.tooltipTimer = setTimeout(this.show, 200);
+        },
+
+        moveTooltip: function(event) {
+            this.tooltip.css(this.getCoords(event.pageX, event.pageY));
+        },
+
+        hideTooltip: function() {
+            clearTimeout(this.tooltipTimer);
+            // Wait for a 200ms before hide the tooltip to avoid blinking when
+            // the item contains nested elements.
+            this.tooltipTimer = setTimeout(this.hide, 200);
+        },
+
+        destroy: function () {
+            this.tooltip.remove();
+            // Unbind all delegated event handlers n the ".TooltipManager"
+            // namespace.
+            this.element.off('.TooltipManager', this.SELECTOR);
+        }
+    };
+
+    return TooltipManager;
+}); // end require()
